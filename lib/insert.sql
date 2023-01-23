88714377537de9529de82cc1adce090968d15097
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2023-01-23 19:40:43 +0000

    commit tree 8eb231b2a615ad60fbb1e742f7433a7ba8e288b7

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex) VALUES ('Mr. Chocolate', 5, 'M');
+INSERT INTO bears (name, age, sex) VALUES ('Rowdy', 6, 'M');
+INSERT INTO bears (name, age, sex) VALUES ('Tabitha', 7,  'F');
+INSERT INTO bears (name, age, sex) VALUES ('Sergeant Brown', 8, 'F');
+INSERT INTO bears (name, age, sex) VALUES ('Melissa',9, 'M');
+INSERT INTO bears (name, age, sex) VALUES ('Grinch', 8, 'F');
+INSERT INTO bears (name, age, sex) VALUES ('Wendy', 7, 'M');
+INSERT INTO bears (name, age, sex) VALUES (NULL, 6, 'F');
